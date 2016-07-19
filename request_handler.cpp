267cbb442a12a94f0b9ca0850ea04ba0c1f8e7ba
tamanrasset <kossov.ev@gmail.com>
2016-07-19 13:55:47 +0300

    Fixed path and url parsing

--- request_handler.cpp
+++ request_handler.cpp
@@ -13,6 +13,10 @@ namespace server3 {
 request_handler::request_handler(const std::string& doc_root)
   : doc_root_(doc_root)
 {
+    if(doc_root_[doc_root_.size() - 1] == '/')
+    {
+        doc_root_ = doc_root_.substr(0, doc_root_.size()-1);
+    }
 }
 
 void request_handler::handle_request(const request& req, reply& rep)
@@ -71,41 +75,47 @@ void request_handler::handle_request(const request& req, reply& rep)
 
 bool request_handler::url_decode(const std::string& in, std::string& out)
 {
-  out.clear();
-  out.reserve(in.size());
-  for (std::size_t i = 0; i < in.size(); ++i)
-  {
-    if (in[i] == '%')
+    out.clear();
+    out.reserve(in.size());
+    auto pos=in.find('?',0);
+    if(pos!=-1)
     {
-      if (i + 3 <= in.size())
-      {
-        int value = 0;
-        std::istringstream is(in.substr(i + 1, 2));
-        if (is >> std::hex >> value)
-        {
-          out += static_cast<char>(value);
-          i += 2;
-        }
-        else
-        {
-          return false;
-        }
-      }
-      else
-      {
-        return false;
-      }
+        out = in.substr(0, pos);
     }
-    else if (in[i] == '+')
-    {
-      out += ' ';
-    }
-    else
-    {
-      out += in[i];
-    }
-  }
-  return true;
+
+  //for (std::size_t i = 0; i < in.size(); ++i)
+  //{
+    //if (in[i] == '%')
+    //{
+      //if (i + 3 <= in.size())
+      //{
+        //int value = 0;
+        //std::istringstream is(in.substr(i + 1, 2));
+        //if (is >> std::hex >> value)
+        //{
+          //out += static_cast<char>(value);
+          //i += 2;
+        //}
+        //else
+        //{
+          //return false;
+        //}
+      //}
+      //else
+      //{
+        //return false;
+      //}
+    //}
+    //else if (in[i] == '+')
+    //{
+      //out += ' ';
+    //}
+    //else
+    //{
+      //out += in[i];
+    //}
+  //}
+    return true;
 }
 
 } // namespace server3
