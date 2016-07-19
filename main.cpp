8c33eb21d66b5fd0ec126973d8b63e728aa235d3
tamanrasset <kossov.ev@gmail.com>
2016-07-19 12:12:42 +0300

    Rollback to compilation success

--- main.cpp
+++ main.cpp
@@ -2,21 +2,6 @@
 #include <string>
 #include <unistd.h>
 #include "server.hpp"
-#include <boost/log/trivial.hpp>
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-
-namespace logging = boost::log;
-namespace src = boost::log::sources;
-namespace sinks = boost::log::sinks;
-namespace keywords = boost::log::keywords;
-
 
 #define NUM_THREADS 4
 
